a156c49352c179a27244fb8d0d8dde8c3467328e
PaulSebastianKrus <paulsebastiankrus@gmail.com>
2024-04-16 21:45:09 +0200

    redirect localhost:8080 to login page

--- src/main/java/com/example/wishlist/controller/UserController.java
+++ src/main/java/com/example/wishlist/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class UserController {
@@ -20,6 +21,13 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+
+
+    @GetMapping("/")
+    public RedirectView redirectToLogin() {
+        return new RedirectView("/login");
+    }
 
 
     @GetMapping("/register")
